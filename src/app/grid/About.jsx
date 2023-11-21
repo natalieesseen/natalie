ef01cd9b54243aecb87c064e3357365d2a106a27
natalieesseen <natalieesseen@gmail.com>
2023-11-21 18:12:33 +0100

    add some content to About

--- src/app/grid/About.jsx
+++ src/app/grid/About.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from './about.module.scss'
 
 const About = () => {
   return (
     <div className={styles.about}>
+      <Image src='/pfp.png' alt='portrait' className={styles.pfp} width={100} height={100} />
       <p>
         Hej 👋 I'm Natalie, a web developer from Sweden.
       </p>
+      <ul className={styles.btns}>
+        <li className={styles.btn}>
+          <a href="https://github.com/natalieesseen">GitHub</a>
+        </li>
+        <li className={styles.btn}>
+          <a href="https://linkedin.com/in/natalieesseen">LinkedIn</a>
+        </li>
+      </ul>
     </div>
   )
 }
